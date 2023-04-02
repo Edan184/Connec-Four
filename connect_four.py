4dc4eebae790db8a00107678cc73c3ba0d7d5201
edan184 <edan184@hotmail.com>
2023-04-01 18:06:53 -0700

    initial commit

---
+++ connect_four.py
@@ -0,0 +1,10 @@
+import asyncio, websockets
+
+@app.route('/connect_four', methods=['GET'])
+def connect_four():
+    if session_info() is not False:
+        # Checks for session cookie, if not present, redirects to login
+        payload = session_info()[0][0]
+        return render_template('connect_four.html', title='Connect Four!', ip=ip_query(), user=payload[1], name=payload[2])
+    else:
+        return redirect(url_for('login'))
